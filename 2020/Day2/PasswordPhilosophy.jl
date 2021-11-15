2b23be0fcb6583d45f17157ef4fc21330e17ceff
Altair Penubaku <manav.prasad.27@gmail.com>
2021-11-15 22:13:28 +0100

    implemented solution for part 2 of day 2 puzzle from 2020

--- 2020/Day2/PasswordPhilosophy.jl
+++ 2020/Day2/PasswordPhilosophy.jl
@@ -7,10 +7,10 @@ function ParseInputForBounds(StringInput)
     end
 end
 
-PasswordList = readdlm("PuzzleInput.txt")
-
+PasswordList = readdlm("PuzzleInput.txt");
 password_correct_counter = 0;
 
+# Part 1 : Check if the number of occurrences of a letter is within a specified range
 for i in 1:size(PasswordList)[1]
     letter_counter = 0;
     for j in 1:length(PasswordList[i,3])
@@ -19,10 +19,20 @@ for i in 1:size(PasswordList)[1]
         end
     end
 
-    bounds = ParseInputForBounds(PasswordList[i,1]);
+    global bounds = ParseInputForBounds(PasswordList[i,1]);
     if(letter_counter >= bounds[1] && letter_counter <= bounds[2])
         global password_correct_counter = password_correct_counter + 1;
     end
 end
+print("Answer to Part 1 :",password_correct_counter,"\n");
 
-print(password_correct_counter);
+# Part 2 : Check if the letter only occurs in one of two positions in the password
+password_correct_counter = 0;
+for i in 1:size(PasswordList)[1]
+    global positions = ParseInputForBounds(PasswordList[i,1]);
+    if(xor(PasswordList[i,3][positions[1]] == PasswordList[i,2][1],PasswordList[i,3][positions[2]] == PasswordList[i,2][1]))
+        global password_correct_counter = password_correct_counter + 1;
+    end    
+end
+
+print("Answer to Part 2 :",password_correct_counter,"\n");
