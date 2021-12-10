0c459756d538b5d0faa5d9226af48a7cc8f95cca
ManavPenubaku <manav.prasad.27@gmail.com>
2021-12-10 08:41:40 +0100

    tried a solution for part 2 that doesn't work

--- Day24/LobbyLayout.jl
+++ Day24/LobbyLayout.jl
@@ -41,3 +41,33 @@ end
 
 black_tiles,TileDict = FlipTiles(lines)
 println("Solution to Part 1 is : ",black_tiles)
+
+function FlipTilesPart2(TileDict)
+    black_tile_indices = reduce(hcat,(collect(keys(TileDict))))
+    FloorExhibitMain = zeros(Int,250,500)
+    black_tile_indices = black_tile_indices .+ [125;251]
+    black_tile_indices = collect(transpose(black_tile_indices))
+    for n in 1:size(black_tile_indices,1)
+        FloorExhibitMain[black_tile_indices[n,1],black_tile_indices[n,2]] = 1
+    end
+    
+    day_number = 0
+    while day_number < 100
+        FloorExhibit = deepcopy(FloorExhibitMain)
+        for i in 2:1:249
+            for j in 3:2:498
+                neighbor_tiles = [FloorExhibit[i+1,j+1] FloorExhibit[i-1,j-1] FloorExhibit[i,j+2] FloorExhibit[i,j-2] FloorExhibit[i-1,j+1] FloorExhibit[i+1,j-1]]
+                black_tile_neighbors = sum(neighbor_tiles .== 1)
+                if (FloorExhibit[i,j] == 0 && black_tile_neighbors == 2)
+                    FloorExhibitMain[i,j] = 1;
+                elseif (FloorExhibit[i,j] == 1 && (black_tile_neighbors == 0 || black_tile_neighbors > 2))
+                    FloorExhibitMain[i,j] = 0;
+                end
+            end
+        end
+        day_number+=1
+    end
+    return sum(FloorExhibitMain .== 1)
+end
+
+sol2 = FlipTilesPart2(TileDict)
