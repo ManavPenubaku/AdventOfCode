eea1966a7f2adaa43886bdc33911e1ae06859765
ManavPenubaku <manav.prasad.27@gmail.com>
2021-12-11 20:31:50 +0100

    solved day 18s puzzle

--- Day18/OperationOrder.jl
+++ Day18/OperationOrder.jl
@@ -2,35 +2,117 @@ file = open("input.txt")
 lines = readlines(file)
 equations = join.(split.(lines," "));
 
-function EvaluateExpressions(equations)
-    answers = Array{Int64}(undef,length(lines))
-    temp_eval = 0;
-    bracket_indices = 0;
-    for expression in 1:length(lines)
-        open_bracket_indices = findall(x->x.=='(',lines[expression])
-        closed_bracket_indices = findall(x->x.==')',lines[expression])
-        
-        
-        # current_line = lines[expression]
-        # position = 1;
-        # while position < length(current_line)
-        #     if current_line[position] == '('
-        #         answers[position] = temp_eval
-        #         temp_eval = 0
-        #         position += 1;
-        #     elseif current_line[position] == ')'
-
-        #     elseif current_line[position] == '+'
-        #         temp_eval = temp_eval + current_line[position+1]
-        #         position+=2;
-        #     elseif current_line[position] == '*'
-        #         temp_eval = temp_eval * current_line[position+1]
-        #     elseif temp_eval == 0
-        #         temp_eval = parse(Int,current_line[position])
-        #     end
-        # end
+function ComputeExpression(equation)
+    count = 1
+    value = 0
+    while count < length(equation)
+        if equation[count] == '('
+            temp_value,temp_count = ComputeExpression(equation[count+1:end])
+            if count >= 2 && equation[count-1] == '+'
+                value += temp_value
+            elseif count >= 2 && equation[count-1] == '*'
+                value *= temp_value
+            else
+                value = temp_value
+            end
+            count += temp_count + 1
+        elseif equation[count] == ')'
+            return value,count
+        elseif equation[count] == '*' && equation[count+1] != '('
+            value = value * parse(Int,equation[count+1])
+            count+=2
+        elseif equation[count] == '+'&& equation[count+1] != '('
+            value = value + parse(Int,equation[count+1])
+            count+=2
+        elseif isdigit(equation[count])
+            value = parse(Int,equation[count])
+            count+=1
+        else
+            count+=1
+        end
     end
-    return bracket_indices
+    return value,count
 end
 
-answers = EvaluateExpressions(lines)
+function ConductAddition(parsed_equation_array)
+    while true
+        indices = findall(x->x == '+',parsed_equation_array)
+        addition_indices = indices[findall(x->(isdigit(string(parsed_equation_array[x-1])[1]) && isdigit(string(parsed_equation_array[x+1])[1])),indices)]
+        if (isempty(addition_indices))
+            break;
+        else
+            first_addition_index = addition_indices[1]
+            parsed_equation_array[first_addition_index-1] = parsed_equation_array[first_addition_index-1] + parsed_equation_array[first_addition_index+1]
+            deleteat!(parsed_equation_array,first_addition_index:first_addition_index+1)
+        end
+    end
+    return parsed_equation_array
+end
+
+function ConductMultiplication(parsed_equation_array)
+    while true
+        indices = findall(x->x == '*',parsed_equation_array)
+        mult_indices = indices[findall(x->(isdigit(string(parsed_equation_array[x-1])[1]) && isdigit(string(parsed_equation_array[x+1])[1])),indices)]
+        if (isempty(mult_indices))
+            break;
+        else
+            first_mult_index = mult_indices[1]
+            parsed_equation_array[first_mult_index-1] = parsed_equation_array[first_mult_index-1] * parsed_equation_array[first_mult_index+1]
+            deleteat!(parsed_equation_array,first_mult_index:first_mult_index+1)
+        end
+    end
+    return parsed_equation_array
+end
+
+function ComputeExpression2(equation_array)
+    parsed_equation_array = deepcopy(equation_array)
+    while true
+        open_bracket = findfirst(x->x == '(',parsed_equation_array)
+        if (isnothing(open_bracket))
+            parsed_equation_array = ConductAddition(parsed_equation_array)
+            parsed_equation_array = ConductMultiplication(parsed_equation_array)
+            break
+        end
+
+        next_open_bracket = findfirst(x->x == '(',parsed_equation_array[open_bracket+1:end])
+        close_bracket = findfirst(x->x == ')',parsed_equation_array[open_bracket+1:end])
+
+        if  isnothing(next_open_bracket) || close_bracket < next_open_bracket
+            temp_array = ConductAddition(parsed_equation_array[open_bracket+1:open_bracket+close_bracket-1])
+            parsed_equation_array[open_bracket] = ConductMultiplication(temp_array)[1]
+            deleteat!(parsed_equation_array,open_bracket+1:open_bracket+close_bracket)
+        else
+            next_close_bracket = findfirst(x->x == ')',parsed_equation_array[open_bracket+next_open_bracket+1:end])
+            parsed_equation_array[open_bracket+next_open_bracket] = ComputeExpression2(parsed_equation_array[open_bracket+next_open_bracket:open_bracket+next_open_bracket+next_close_bracket])
+            deleteat!(parsed_equation_array,next_open_bracket+open_bracket+1:open_bracket+next_close_bracket+next_open_bracket)
+        end
+        
+    end
+    return parsed_equation_array[1]
+end
+
+function EvaluateExpressions(equations)
+    answers1 = Array{Int64}(undef,length(equations))
+    answers2 = Array{Int64}(undef,length(equations))
+    for n in 1:length(lines)
+        current_equation = equations[n]
+        answers1[n],counter = ComputeExpression(current_equation)
+        current_equation_array = String.(split(current_equation,""))
+        parsed_equation_array = Array{Any}(undef,length(current_equation_array))
+        for n in 1:length(current_equation_array)
+            if (isdigit(current_equation_array[n][1]))
+                parsed_equation_array[n] = parse(Int,current_equation_array[n])
+            else
+                parsed_equation_array[n] = current_equation_array[n][1]
+            end
+        end
+        answers2[n] = ComputeExpression2(parsed_equation_array)
+    end
+    return sum(answers1),sum(answers2)
+end
+
+
+
+sum_p1,sum_p2 = EvaluateExpressions(equations)
+println("Solution to Part 1 is : ",sum_p1)
+println("Solution to Part 2 is : ",sum_p2)
--- Day24/LobbyLayout.jl
+++ Day24/LobbyLayout.jl
@@ -45,8 +45,8 @@ println("Solution to Part 1 is : ",black_tiles)
 function FlipTilesPart2(TileDict)
     black_tile_indices = reduce(hcat,(collect(keys(TileDict))))
     tile_values = collect(values(TileDict))
-    FloorExhibitMain = zeros(Int,250,500)
-    black_tile_indices = black_tile_indices .+ [125;251]
+    FloorExhibitMain = zeros(Int,150,400)
+    black_tile_indices = black_tile_indices .+ [75;201]
     black_tile_indices = collect(transpose(black_tile_indices))
     for n in 1:size(black_tile_indices,1)
         FloorExhibitMain[black_tile_indices[n,1],black_tile_indices[n,2]] = tile_values[n]
@@ -54,13 +54,13 @@ function FlipTilesPart2(TileDict)
     day_number = 1
     while day_number <= 100
         FloorExhibit = deepcopy(FloorExhibitMain)
-        for i in 2:1:249
+        for i in 2:1:149
             if (isodd(i))
                 start_j = 3
             else
                 start_j = 4
             end
-            for j in start_j:2:498
+            for j in start_j:2:398
                 neighbor_tiles = [FloorExhibit[i+1,j+1] FloorExhibit[i-1,j-1] FloorExhibit[i,j+2] FloorExhibit[i,j-2] FloorExhibit[i-1,j+1] FloorExhibit[i+1,j-1]]
                 black_tile_neighbors = sum(neighbor_tiles .== 1)
                 if (FloorExhibit[i,j] == 0 && black_tile_neighbors == 2)
@@ -76,3 +76,4 @@ function FlipTilesPart2(TileDict)
 end
 
 sol2 = FlipTilesPart2(TileDict)
+println("Solution to Part 2 is : ",sol2)
